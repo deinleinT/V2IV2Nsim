ddae03f3b83e863eca3ebaad2e275920579b36cd
Thomas Deinlein <thomas.deinlein@fau.de>
2020-11-14 15:20:56 +0100

    added make and clean scripts

---
+++ 5G-SIM-V2IN/cleanAll.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd inet
+make cleanall
+cd ..
+cd veins
+make cleanall
+cd ..
+cd veins_inet3
+make cleanall
+cd ..
+cd lteNR
+make cleanall
+cd ..
+
---
+++ 5G-SIM-V2IN/makeRelease.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd inet
+make makefiles
+make MODE=release -j16
+cd ..
+cd veins
+make makefiles
+make MODE=release -j16
+cd ..
+cd veins_inet3
+make makefiles
+make MODE=release -j16
+cd ..
+cd lteNR
+make makefiles
+make MODE=release -j16
+cd ..
+
